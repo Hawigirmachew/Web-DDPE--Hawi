040b51a76448d2ff916f5a300449f0878075ba77
Hawigirmachew <88828065+Hawigirmachew@users.noreply.github.com>
2022-01-30 06:46:53 -0800

    Add files via upload

---
+++ file/script.js
@@ -0,0 +1,9 @@
+function highlightElement(elem){
+    elem.style.color ="blue";
+    elem.style.background = "lightgray";
+
+} 
+function handelOnblurFuction(elem){
+    elem.style.color="red";
+    elem.style.background = "lightblue";
+}
